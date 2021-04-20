8b89464d7dc6fdffa95a6a1b463179b2da88d3b7
cplessis <clement.plessis@outlook.fr>
2021-04-20 15:58:46 +0200

    updated DESCRIPTION

--- src/q2_mkrefdb/__main__.py
+++ src/q2_mkrefdb/__main__.py
@@ -2,7 +2,7 @@ import argparse, sys, getpass, datetime, platform
 from . import fastafilter as ff
 from . import utils
 
-__doc__ =  """This package is release by the Research and Development Institute 
+__doc__ =  """This package is released and maintained by the Research and Development Institute 
 for Agri-Environnement (IRDA - Quebec). More informations at https://github.com/cplessis/q2_mkrefdb."""
 
 def get_arguments():
