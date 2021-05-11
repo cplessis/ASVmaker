43d743a957e7e815a4b3f7af67593d9f9cfbc7ce
cplessis <clement.plessis@outlook.fr>
2021-05-11 18:30:49 +0200

    moved database export function into export.py

--- src/q2_mkrefdb/database.py
+++ src/q2_mkrefdb/database.py
@@ -1,5 +1,6 @@
+from os import sep
 import re, time
-from . import utils, filter, lineage, stats, edit
+from . import utils, filter, lineage, stats, edit, export
 from .pcr import amplify
 from progress.bar import FillingSquaresBar
 from progress.spinner import PixelSpinner
@@ -458,290 +459,97 @@ class Database:
 #######################################################################################################################
 
     def clean_dataset(self, wanted_genus1, wanted_genus2, unverified_bool):
-        """Keep in the dataset only the specified genus. The unverified bool remove all
-        the species which are ended by '.' such as 'Fusarium sp.' if False is specified. 
-
-        Args:
-            wanted_genus1 (string): genus to keep 1
-            wanted_genus2 (string): genus to keep 2
-            unverified_bool (bool): False to to remove unverfied ('.'), else True.  
-        """        
         self.seq_dict = filter.clean_dataset(self, wanted_genus1, wanted_genus2, unverified_bool)
         self.__update_data()
+    clean_dataset.__doc__ = filter.clean_dataset.__doc__
 
     def del_redund_seq_glob(self):
-        """Remove all the redundant sequence globaly. All the sequences are compared two by two, 
-        if they are 100% identical one of them is removed.
-        """
         self.seq_dict = filter.del_redund_seq_glob(self)
         self.__update_data()
+    del_redund_seq_glob.__doc__ = filter.del_redund_seq_glob.__doc__
 
     def del_redund_seq_tax(self):
-        """Remove all the redundant sequence by taxon. All the sequences are compared two by two in each group of taxon only, 
-        if they are 100% identical one of them is removed.
-        """        
         self.seq_dict = filter.del_redund_seq_tax(self)
         self.__update_data()
+    del_redund_seq_tax.__doc__ = filter.del_redund_seq_tax.__doc__
 
     def del_na_amplicons(self):
-        """Remove all the sequences which have no amplicon ('NA').
-        """        
         self.seq_dict = filter.del_na_amplicons(self)
         self.__update_data()
+    del_na_amplicons.__doc__ = filter.del_na_amplicons.__doc__
 
     def del_redund_ampli_glob(self):
-        """Remove all the redundant amplicons globaly. All the amplicons are compared two by two, 
-        if they are 100% identical one of them is removed.
-        """        
         self.seq_dict = filter.del_redund_ampli_glob(self)
         self.__update_data()
+    del_redund_ampli_glob.__doc__ = filter.del_redund_ampli_glob.__doc__
 
     def del_redund_ampli_tax(self):
-        """Remove all the redundant amplicons by taxon. All the amplicons are compared two by two in each group of taxon only, 
-        if they are 100% identical one of them is removed.
-        """        
         self.seq_dict = filter.del_redund_ampli_tax(self)
         self.__update_data()
+    del_redund_ampli_tax.__doc__ = filter.del_redund_ampli_tax.__doc__
 
     def group_by_complex(self):
-        """Change the lineage and taxon of all species which are in a species complex.
-        The taxon name become the complex name and the lineage stop at this level. 
-        By this way, species are gathered in complex.  
-        """        
         self.access_dict = filter.group_by_complex(self)
         self.__update_data()
+    group_by_complex.__doc__ = filter.group_by_complex.__doc__
+
+#######################################################################################################################
+#################         EDIT           ##############################################################################
+#######################################################################################################################
+
+    def remove_by_id(self, id_list_csv):        
+        self.seq_dict = edit.remove_by_id(self, id_list_csv)
+        self.__update_data()
+    remove_by_id.__doc__ = edit.remove_by_id.__doc__
+
+    def rename_by_id(self, id_list_csv):
+        self.access_dict = edit.rename_by_id(self, id_list_csv)
+        self.__update_data()
+    rename_by_id.__doc__ = edit.rename_by_id.__doc__
+
+    def group_by_id(self, shared_ext_csv):
+        group = edit.group_by_id(self, shared_ext_csv)
+        self.access_dict = group[0]
+        self.seq_dict = group[1]
+        self.__update_data()
+    group_by_id.__doc__ = edit.group_by_id.__doc__
 
 #######################################################################################################################
 #################         EXPORT         ##############################################################################
 #######################################################################################################################
 
     def export_tax_id_txt(self, output_file_name, separator):
-        """Export the Taxonomic Table for Qiime2. Col1 with accession number and 
-        Col2 with Lineage.
-
-        Args:
-            output_file_name (string): path to output file
-            separator (string): column separator 
-        """        
-        with open(output_file_name, "w") as taxon_file:
-            for access_nb in self.access_dict:
-                lineage = self.get_lineage(access_nb)
-                if lineage == "NA": lineage = ""
-                taxon_file.write(access_nb+separator+lineage+self.get_taxon(access_nb)+"\n")
-        print("   ==> Taxonomic table successfully exported.")
+        export.export_tax_id_txt(self, output_file_name, separator)
+    export_tax_id_txt.__doc__ = export.export_tax_id_txt.__doc__
 
     def export_seq_fasta(self, output_file_name, fasta_type):
-        """Export the sequences FASTA file. Each sequence is written on a single line 
-        if 'qiime' is specified or multiple lines (70bp) if 'ncbi' is specified.
-        The description is the accession number of the sequence. 
+        export.export_seq_fasta(self, output_file_name, fasta_type)
+    export_seq_fasta.__doc__ = export.export_seq_fasta.__doc__
 
-        Args:
-            output_file_name (string): path to output file
-            fasta_type (string): 'qiime' or 'ncbi'
-        """        
-        if fasta_type == "qiime": self.__seq_qiime_fasta(output_file_name)
-        elif fasta_type == "ncbi": self.__seq_ncbi_fasta(output_file_name)
-        elif fasta_type == "phylo": self.__seq_phylo_fasta(output_file_name)
-        else: print("Please reload this function with 'qiime', 'ncbi' or 'phylo' argument.")
-  
-    def __seq_ncbi_fasta(self, output_file_name):
-        """Called by export_seq_fasta if 'ncbi' is specified.
-        This function makes the file.
-
-        Args:
-            output_file_name (string): path to output file
-        """        
-        with open(output_file_name, "w") as fasta_file:
-            for seq_name in self.seq_dict:
-                fasta_file.write(seq_name+"\n")
-                seq_list = re.findall('.{1,70}', self.seq_dict[seq_name])
-                for seq in seq_list:
-                    fasta_file.write(seq+"\n")
-        print("   ==> Sequences variants FASTA file susccessfully exported.")
-
-    def __seq_qiime_fasta(self, output_file_name):
-        """Called by export_seq_fasta if 'qiime' is specified.
-        This function makes the file.
-
-        Args:
-            output_file_name (string): path to output file
-        """        
-        with open(output_file_name, "w") as fasta_file:
-            for access_nb in self.access_dict:
-                fasta_file.write(">"+access_nb+"\n"+self.get_sequence(access_nb)+"\n")
-        print("   ==> Sequences variants FASTA file susccessfully exported.")
-    
-    def __seq_phylo_fasta(self, output_file_name):
-        """Called by export_seq_fasta if 'phylo' is specified.
-        This function makes the file.
-
-        Args:
-            output_file_name (string): path to output file
-        """        
-        with open(output_file_name, "w") as fasta_file:
-            for access_nb in self.access_dict:
-                fasta_file.write(">"+access_nb+"_"+self.get_taxon(access_nb)+"\n"+\
-                    self.get_sequence(access_nb)+"\n")
-        print("   ==> Sequences variants FASTA file susccessfully exported.")
-        
     def export_taxon_list(self, output_file_name):
-        """Export the taxon list with their number of ASV. Col1 = taxon, col2 = nb of ASV. 
-
-        Args:
-            output_file_name (string): path to the output file
-        """        
-        taxon_list = []
-        for taxon in self.taxon_dict:
-            taxon_list.append(taxon+"\t"+str(len(self.taxon_dict[taxon]))) 
-        utils.export_list_csv(sorted(taxon_list), output_file_name)
-        print("   ==> Taxon list susccessfully exported.")
+        export.export_taxon_list(self, output_file_name)
+    export_taxon_list.__doc__ = export.export_taxon_list.__doc__
 
     def export_ampli_fasta(self, output_file_name, fasta_type):
-        """Export the amplicon FASTA file. Each amplicon is written on a single line 
-        if 'qiime' is specified or multiple lines (70bp) if 'ncbi' is specified.
-        The description is the accession number of the amplicon. 
-
-        Args:
-            output_file_name (string): path to output file
-            fasta_type (string): 'qiime' or 'ncbi'
-        """        
-        if fasta_type == "qiime": self.__ampli_qiime_fasta(output_file_name)
-        elif fasta_type == "ncbi": self.__ampli_ncbi_fasta(output_file_name)
-        elif fasta_type == "phylo": self.__ampli_phylo_fasta(output_file_name)
-        else: print("Please reload this function with 'qiime' or 'ncbi' argument.")
-
-    def __ampli_ncbi_fasta(self, output_file_name):
-        """Called by export_ampli_fasta if 'ncbi' is specified.
-        This function makes the file.
-
-        Args:
-            output_file_name (string): path to the output file
-        """        
-        with open(output_file_name, "w") as amplicon_file:
-            for access_nb in self.access_dict:
-                amplicon_file.write(">"+access_nb+"_"+self.get_taxon(access_nb)+"\n")
-                seq_list = re.findall('.{1,70}', self.get_amplicon(access_nb))
-                for seq in seq_list:
-                    amplicon_file.write(seq+"\n")
-        print("   ==> Amplicon FASTA file susccessfully exported.")
-
-    def __ampli_qiime_fasta(self, output_file_name):
-        """Called by export_ampli_fasta if 'ncbi' is specified.
-        This function makes the file.
-
-        Args:
-            output_file_name (string): path to output file
-        """        
-        with open(output_file_name, "w") as amplicon_file:
-            for access_nb in self.access_dict:
-                amplicon_file.write(">"+access_nb+"\n"+self.get_amplicon(access_nb)+"\n")
-        print("   ==> Amplicon FASTA file susccessfully exported.")
-    
-    def __ampli_phylo_fasta(self, output_file_name):
-        """Called by export_ampli_fasta if 'phylo' is specified.
-        This function makes the file.
-
-        Args:
-            output_file_name (string): path to output file
-        """        
-        with open(output_file_name, "w") as amplicon_file:
-            for access_nb in self.access_dict:
-                amplicon_file.write(">"+access_nb+"_"+self.get_taxon(access_nb)+"\n"+\
-                    self.get_amplicon(access_nb)+"\n")
-        print("   ==> Amplicon FASTA file susccessfully exported.")
+        export.export_ampli_fasta(self, output_file_name, fasta_type)
+    export_ampli_fasta.__doc__ = export.export_ampli_fasta.__doc__
 
     def export_shared_ampli(self, output_file_name, threshold):
-        """Export a file with all the accession number which have an identical amplicon.
-        Each interaction is represented as 'accession_specie1 <=> accession_specie2'.
-        The symbol '<=>' means there a common amplicon between the two sequences. 
-
-        Args:
-            output_file_name (string): path to the output file
-        """
-        sa_ampli = self.get_shared_amplicons(threshold)
-        utils.export_list_csv(sa_ampli[0], output_file_name)
-        utils.export_dict_csv2(sa_ampli[1], "_ext.".join(output_file_name.rsplit(".", 1)), "\t")        
-        print("   ==> Shared amplicons list successfully exported.")
+        export.export_shared_ampli(self, output_file_name, threshold)
+    export_shared_ampli.__doc__ = export.export_shared_ampli.__doc__
     
-    def export_complex_dict(self, outputfile_name, separator):
-        """Export a file with all the complex line per line. On each line, 
-        all the accession number related to this complex are written and also
-        all the original species in the complex (all the species which have now the name 
-        of the complex instead of there original name).
-        Exemple: Fusarium Fujikori complex  {'access': [AJ567689, AK787990], 'taxon': [Fusarium_fujikoroi] }
-
-        Args:
-            outputfile_name (string): path to the output file
-            separator (string): seprator between the complex name and the complex infos
-        """        
-        utils.export_dict_csv(self.complex_dict, outputfile_name, separator)
-        print("   ==> Complex dictionnary successfully exported.")
+    def export_complex_dict(self, output_file_name, separator):
+        export.export_complex_dict(self, output_file_name, separator)
+    export_complex_dict.__doc__ = export.export_complex_dict.__doc__
 
     def export_modified_tax(self, output_file_name):
-        """Export the list of all taxon which have been modified during the
-        lineage construction. For example: 'Gibberella_sp. => Fusarium_oxysporum'.
-
-        Args:
-            output_file_name (string): path to output file
-        """        
-        utils.export_list_csv(self.__modified_taxon, output_file_name)
-        print("   ==> Modified taxon list successfully exported.")
+        export.export_modified_tax(self, output_file_name)
+    export_modified_tax.__doc__ = export.export_modified_tax.__doc__
 
     def export_access_dict(self, output_file_name):
-        """Export the access_dict with all the informations of the data to JSON format.
-        WARNING : You must write the file extension as '.json' to avoir Errors.
-
-        Args:
-            output_file_name (string): path to output file
-        """
-        self.access_dict["infos"] = {
-            "file_name":self.file_name,
-            "origin_db":self.origin_database,
-            "fw_mis_tol":self.fw_mismatch_tol,
-            "rv_mis_tol":self.rv_mismatch_tol,
-            "trim_prim":self.trim_primers,
-            "fw_prim":self.forward_primer_list,
-            "rv_prim":self.reverse_primer_list,
-            "sa_threshold":self.sa_threshold}     
-        utils.save_as_json(self.access_dict, output_file_name)
-        print("   ==> Access_dict successfully exported.")
-
-
-
-#######################################################################################################################
-#################         EDIT           ##############################################################################
-#######################################################################################################################
-
-    def remove_by_id(self, id_list_csv):
-        """Remove all the sequences with IDs on id_list_csv.
-        The id_list_csv FILE must be as one line = 'seq_id'. 
-
-        Args:
-            id_list_csv (str): ID list CSV or TXT file
-        """        
-        self.seq_dict = edit.remove_by_id(self, id_list_csv)
-        self.__update_data()
-
-    def rename_by_id(self, id_list_csv):
-        """Rename all the sequences with IDs on id_list_csv.
-        The id_list_csv FILE must be as one line = 'seq_id   new_taxon_name'. 
-
-        Args:
-            id_list_csv (str): ID list CSV or TXT file
-        """ 
-        self.access_dict = edit.rename_by_id(self, id_list_csv)
-        self.__update_data()
-
-    def group_by_id(self, shared_ext_csv):
-        """Group all the sequences with IDs on shared_ext_csv on a commune taxon name.
-        The taxon name will become the SA_id of the group. Only one amplicon will represent
-        the group after this command is runned. The shared_ext_csv FILE must be the one
-        generated with EXPORT shared ampl.
-
-        Args:
-            shared_ext_csv (str): shared_ext FILE generated with EXPORT shared ampl.
-        """ 
-        group = edit.group_by_id(self, shared_ext_csv)
-        self.access_dict = group[0]
-        self.seq_dict = group[1]
-        self.__update_data()
+        export.export_access_dict(self, output_file_name)
+    export_access_dict.__doc__ = export.export_access_dict.__doc__
+    
+    def export_phylo_annot(self, output_file):
+        export.phylo_annotation(self, output_file)
+    export_phylo_annot.__doc__ = export.phylo_annotation.__doc__
