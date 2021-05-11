a98e7a0359170be935296a3c6dce11fdc5804713
cplessis <clement.plessis@outlook.fr>
2021-05-11 16:37:21 +0200

    created export.py with function for phylo annotation file

---
+++ src/q2_mkrefdb/export.py
@@ -0,0 +1,12 @@
+from . import utils
+import q2_mkrefdb.database as database
+
+def phylo_annotation(Database, output_file):
+    with open(output_file, "w") as file:
+        file.write("TREE_COLORS\nSEPARATOR TAB\nDATA\n")
+        for taxon in Database.taxon_dict:
+            if taxon.split("_")[1][0] != "S": 
+                color = utils.random_color()
+                for access_nb in Database.taxon_dict[taxon]:
+                    file.write(access_nb+"_"+taxon+"\tlabel\trgb"+str(color)+"\tnormal\t1\n")
+                    file.write(access_nb+"_"+taxon+"\tbranch\trgb"+str(color)+"\tnormal\t1\n")
